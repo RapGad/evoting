8499f97099621638f71e51f8e7465839f8e2e2c5
rapgad <jeffreyhayford98@gamil.com>
2025-05-09 15:13:48 +0000

    Placed index js in api folder

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const connectDb = require('./lib/db')
-const authRouter = require('./routes/auth.route')
-const voteRouter = require('./routes/vote.route')
+const connectDb = require('../lib/db')
+const authRouter = require('../routes/auth.route')
+const voteRouter = require('../routes/vote.route')
+const serverless = require('serverless-http');
+
 
 const PORT = process.env.PORT || 5002
 
@@ -15,7 +17,9 @@ app.use(cors())
 app.use('/api/auth' ,authRouter)
 app.use('/api/student', voteRouter)
 connectDb()
-app.listen(PORT, ()=>{
+/* app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
     
-})
+}) */
+module.exports.handler = serverless(app);
+
